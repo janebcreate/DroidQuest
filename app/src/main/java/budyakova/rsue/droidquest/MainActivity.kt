21dfd8599631aa3485d750840dfeb518aa46973f
eugeniabud <evgenia.zrain@mail.ru>
2022-03-20 15:23:03 +0300

    лр4

--- app/src/main/java/budyakova/rsue/droidquest/MainActivity.kt
+++ app/src/main/java/budyakova/rsue/droidquest/MainActivity.kt
@@ -2,32 +2,69 @@ package budyakova.rsue.droidquest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button;
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mTrueButton: Button
     private lateinit var mFalseButton: Button
+    private lateinit var mPreviousButton: ImageButton
+    private lateinit var mNextButton: ImageButton
+    private lateinit var mQuestionTextView: TextView
+    private val mQuestionBank = listOf(
+        Question(R.string.question_android, true),
+        Question(R.string.question_linear, false),
+        Question(R.string.question_service, false),
+        Question(R.string.question_res, true),
+        Question(R.string.question_manifest, true)
+    )
+    private var mCurrentIndex = 0
+
+    fun checkAnswer(userPressedTrue: Boolean) {
+        val answerIsTrue = mQuestionBank[mCurrentIndex].answerTrue
+        val messageResId = if (userPressedTrue == answerIsTrue) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        mQuestionTextView = findViewById(R.id.question_text_view)
         mTrueButton = findViewById(R.id.true_button)
         mTrueButton.setOnClickListener {
-            Toast.makeText(this, R.string.correct_toast,
-                Toast.LENGTH_SHORT).show()
+            checkAnswer(true)
         }
 
         mFalseButton = findViewById(R.id.false_button)
-        mFalseButton.setOnClickListener{
-            Toast.makeText(this, R.string.incorrect_toast,
-                Toast.LENGTH_SHORT).show()
+        mFalseButton.setOnClickListener {
+            checkAnswer(false)
         }
 
+        fun updateQuestion() {
+            val question = mQuestionBank[mCurrentIndex].textResId
+            mQuestionTextView.setText(question)
+        }
 
+        mPreviousButton = findViewById(R.id.previous_button)
+        mPreviousButton.setOnClickListener {
+            mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.size
+            updateQuestion()
 
+            mNextButton = findViewById(R.id.next_button)
+            mNextButton.setOnClickListener {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.size
+                updateQuestion()
+            }
+        }
     }
-}
+    }
+
